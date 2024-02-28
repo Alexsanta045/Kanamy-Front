8941d30c6661db38e14ffe8c91a5ed5f76c0bcb2
Alexander Santa <Alexsanta045@gmail.com>
2024-02-28 11:34:04 -0500

    Adelanto front

--- src/layout/ContentFooter.vue
+++ src/layout/ContentFooter.vue
@@ -9,10 +9,9 @@
         </ul>
       </nav>
       <div class="copyright text-center">
-        &copy; Coded with
-        <i class="fa fa-heart heart"></i> by
-        <a href="https://binarcode.com" target="_blank">BinarCode</a>.
-        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
+        &copy; Coded with Kanamy group S.A.S
+        <!-- <a href="https://binarcode.com" target="_blank">BinarCode</a>.
+        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>. -->
       </div>
     </div>
   </footer>
